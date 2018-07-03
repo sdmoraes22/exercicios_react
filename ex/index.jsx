e8a902e4b8006a13b730d431f9a00fa8344d1e83
Cristiano Moraes <cmoraes.ti@gmail.com>
2018-07-03 18:03:16 -0300

    Configuração inivial do react com o redux para os exercícios

--- ex/index.jsx
+++ ex/index.jsx
@@ -1,7 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { combinedReducers, createStore } from 'redux'
+import { Provider } from 'react-redux'
+
 import Field from './field'
 
+const reducers = combinedReducers({
+    field: () => ({ value: 'Opa' })
+})
+
 ReactDOM.render(
-    <Field  initialValue='Teste' />    
+    <Provider store={ createStore (reducers)} >
+        <Field  initialValue='Teste' />    
+    </Provider>
 ,document.getElementById('app'))
