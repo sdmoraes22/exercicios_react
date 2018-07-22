e34a414fac0e35acfa1787cda8d30549a808c64a
Cristiano Moraes <cmoraes.ti@gmail.com>
2018-07-22 11:31:01 -0300

    Integração final react redux para as propriedades com evolução de estado

---
+++ ex.14/field.js
@@ -0,0 +1,29 @@
+import React, {Component} from 'react'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import { changeValue } from './fieldActions'
+class Field extends Component {
+    render() {
+        return(
+            <div>
+                <label>{this.props.value}</label><br/>
+                <input onChange={this.props.changeValue} value={this.props.value} />
+            </div>
+        )
+       
+    }
+}
+
+
+
+function mapStateToProps(state) {
+    return {
+        value: state.field.value
+    }
+}
+
+function mapDispachtToProps(dispach) {
+    return bindActionCreators({ changeValue }, dispach);
+}
+
+export default connect(mapStateToProps, mapDispachtToProps)(Field)
